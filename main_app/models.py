f9908794eca8ddce0de8d9fceab6da1bc55e7ee4
Jonathan Toye <jontoye@gmail.com>
2022-03-29 17:54:03 -0400

    update Artist model and add Genre model

--- main_app/models.py
+++ main_app/models.py
@@ -7,12 +7,20 @@ https://i.scdn.co/image/ab6761610000e5ebd3cb15a8570cce5a63af63d8
 """
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Artist(models.Model):
     id = models.CharField(primary_key=True, max_length=50)
     name = models.CharField(max_length=100)
     image_url = models.CharField(max_length=300, blank=True)
+    spotify_uri = models.CharField(max_length=200, blank=True)
     spotify_followers = models.IntegerField(blank=True)
-    # genres = models.ManyToManyField(Genre)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.name
